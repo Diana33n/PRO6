8945065a7a9913638ce58d8920c50f34c667491e
Diana33n <146648888+Diana33n@users.noreply.github.com>
2025-02-01 21:43:57 +0200

    Delete PRO6 directory

--- PRO6/src/client/index.js
+++
@@ -1,6 +0,0 @@
-const handleSubmit = require("./js/formhandler");
-
-document.getElementById('processURLButton').addEventListener('click', () => {
-    let URL = document.getElementById('url').value;
-    handleSubmit(URL)
-});
